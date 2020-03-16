fe43d8e5368956a6a50ef3f814cbcda007fc4b8f
bashbash <amjad.bashiti@hotmail.com>
2020-03-16 15:19:28 +0200

    adding documentation to the game

--- TicTacToe.py
+++ TicTacToe.py
@@ -44,11 +44,16 @@ def start():
         player += 1
         counter -= 1
 
-    if counter < 0:
+    if counter < 0:  # if the counter reached to zero so the game ended with draw
         print('The game has end with draw!')
 
 
 def getMove(player):
+    """
+    get valid row and column indexes for next move
+    :param player: the current player number
+    :return: row, column indexes
+    """
     if player % 2 == 0:
         print('\nPlayer one playing now, where to put "X" ?')
     else:
@@ -78,12 +83,22 @@ def getMove(player):
 
 
 def isValid(num):
+    """
+    check if index is valid
+    :param num: index
+    :return:
+    """
     if num < 0 or num >= DIM:
         return False
     return True
 
 
 def printBoard(board):
+    """
+    print current game board
+    :param board:
+    :return:
+    """
     print(end='  ')
     for row in range(DIM):
         print(row, end=' ')
@@ -96,12 +111,26 @@ def printBoard(board):
 
 
 def checkWin(board, row, col):
+    """
+    check if after current move any one has won
+    :param board: game board
+    :param row: row index of current move
+    :param col: column index of current move
+    :return: True / False
+    """
     if checkVertical(board, row, col) or checkHorizontal(board, row, col) or checkDiagonal(board):
         return True
     return False
 
 
 def checkHorizontal(board, row, col):
+    """
+    check if the current player won horizontally
+    :param board: game board
+    :param row: row index of current move
+    :param col: column index of current move
+    :return: True / False
+    """
     currVal = board[row][col]
     if currVal != 'X' and currVal != 'O':
         return False
@@ -112,6 +141,13 @@ def checkHorizontal(board, row, col):
 
 
 def checkVertical(board, row, col):
+    """
+    check if current player won vertically
+    :param board: game board
+    :param row: row index of current move
+    :param col: column index of current move
+    :return: True / False
+    """
     currVal = board[row][col]
     if currVal != 'X' and currVal != 'O':
         return False
@@ -122,6 +158,11 @@ def checkVertical(board, row, col):
 
 
 def checkDiagonal(board):
+    """
+    check if current player won in each of the two main diagonals
+    :param board: game board
+    :return: True / False
+    """
     currVal = board[0][0]
     if currVal != 'X' and currVal != 'O':
         return False
@@ -147,6 +188,15 @@ def checkDiagonal(board):
 
 
 def makeMove(board, row, col, move):
+    """
+    put move char (X or O) in row and column indexes if and only if there is
+    no chars in the specified row and column
+    :param board: game board
+    :param row: row index of current move
+    :param col: column index of current move
+    :param move: curr move value (X or O)
+    :return: True / False
+    """
     if board[row][col] != 'X' and board[row][col] != 'O':
         board[row][col] = move
         return True
